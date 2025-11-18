777507a422182decb6795744bd2f193f1b6e8dd3
a.dernovskiy <dernovski@icloud.com>
2025-11-18 19:19:42 +0700

    corrected SecurityConfig for swagger

--- src/main/java/com/example/bankcards/security/SecurityConfig.java
+++ src/main/java/com/example/bankcards/security/SecurityConfig.java
@@ -29,6 +29,7 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/api/auth/**",
                                 "/swagger-ui/**",
+                                "/swagger-ui.html",
                                 "/v3/api-docs/**",
                                 "/swagger-resources/**",
                                 "/webjars/**"
